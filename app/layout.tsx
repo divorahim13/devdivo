e78060dcf574a40bb41273a338e8ab1594affccd
divorahim13 <divo732@gmail.com>
2026-05-05 21:09:53 +0700

    feat: add TikTok Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,30 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
             ttq.page();
           }(window, document, 'ttq');
         `}</Script>
+
+        {/* Meta (Facebook) Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${process.env.NEXT_PUBLIC_FB_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}</Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
